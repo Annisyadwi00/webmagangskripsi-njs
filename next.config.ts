902c7b25c720e170c34283be06142abf24d9aa08
Annisyadwi00 <annisyadwi0604@gmail.com>
2026-06-25 23:04:08 +0700

    Fix Vercel OOM & update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@
 const nextConfig = {
   // Tambahkan baris ini agar Next.js tidak nge-bundle Sequelize secara berlebihan
   serverExternalPackages: ['sequelize'],
+  swcMinify: false,
+  experimental: {
+    // Membatasi worker yang berjalan bersamaan saat build untuk menghemat RAM
+    memoryBasedWorkersCount: true,
+  }
 };
 
 export default nextConfig;
